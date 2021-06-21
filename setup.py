9b9f5ee1c16d30eaac27ccebadf60f41e0b4a3c2
BulgarinIvt3 <79186706+BulgarinIvt3@users.noreply.github.com>
2021-06-21 11:55:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requirements = ['bs4>=0.0.1', 'requests>=2.25.1']
 
 
 setup(name='wbscrp',
-      version='3.2.1',
+      version='3.2.1.0',
       description='web scraper with methods',
       long_description=open('README.txt').read(),
       packages=['wbscrp'],
